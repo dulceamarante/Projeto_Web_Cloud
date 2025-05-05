f9eeb788059b9aac257c0602c2cc74ce9cdbf1af
ricardo-ourelo <rcooo2@iscte-iul.pt>
2025-05-05 15:11:12 +0100

    o banner já aparece

--- src/components/home/Banner.js
+++ src/components/home/Banner.js
@@ -2,15 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Banner.css';
 
+
 const Banner = () => {
   return (
     <section className="main-banner">
       <div className="banner-container">
-        <img 
-          src="/assets/images/banner/main-banner.jpg" 
-          alt="Nova Coleção" 
-          className="banner-image"
-        />
         <div className="banner-content">
           <button className="banner-button">New in</button>
         </div>
